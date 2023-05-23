814d1187a3e82bb561a9095af2a24a501256c6a8
antonina-dominiak <antonina.dominiak01@gmail.com>
2023-05-23 15:53:22 +0200

    Initial commit

---
+++ azure-blob-storage/variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+  description = "resource group name"
+}
+
+variable "storage_account_name" {
+  description = "storage account name"
+}
+
+variable "container_name" {
+  description = "storage container name"
+}
+
+variable "blob_name" {
+  description = "blob name"
+}
+
+variable "local_file_path" {
+  description = "local file path"
+}
